89b9a5c485c97ede1927af4b7772ce41003cb2ba
Stanley Yu <stanyu88@gmail.com>
2022-09-01 16:43:19 -0400

    Added R implementation of quick sort, but needs more debugging still

--- submissions/week_4/4_improving_quicksort/sorting.R
+++ submissions/week_4/4_improving_quicksort/sorting.R
@@ -3,7 +3,7 @@ get_random_integer <- function(min, max) {
 }
 
 partition3 <- function(arr, l, r) {
-  i = 1
+  i = l
   l_copy = l
   r_copy = r
   pivot_value = arr[l]
@@ -25,6 +25,22 @@ partition3 <- function(arr, l, r) {
   return(c(l_copy, r_copy))
 }
 
+randomized_quick_sort <- function(arr, l, r) {
+  print(arr)
+  if (l >= r) {
+    return()
+  }
+  k = get_random_integer(min = l, max = r) # test random number selection
+  arr_l_copy = arr[l]
+  arr[l] = arr[k] # test random swap under different conditions, like what if we're swapping in the last element.  
+  arr[k] = arr_l_copy # test random swaps
+  pivot_boundaries = partition3(arr = arr, l = l, r = r) # test pivot boundaries
+  arr[pivot_boundaries[1]:pivot_boundaries[2]] # test pivot boundaries.  
+  randomized_quick_sort(arr = arr, l = l, r = (pivot_boundaries[1] - 1))
+  randomized_quick_sort(arr = arr, l = (pivot_boundaries[2] + 1), r = r)
+  return(arr)
+}
+
 # Test 1: Testing Random Integer Function
 random_int = get_random_integer(0, 4)
 if (random_int %in% c(0, 1, 2, 3, 4)) {
@@ -42,3 +58,12 @@ if (pivot_boundaries[1] == 1 & pivot_boundaries[2] == 3) {
   print("Test Failed.  The correct boundaries were not returned, or some other error occurred.")
 }
 
+# Test 3: Testing the Randomized Quick Sort Function
+arr = c(3, 2, 9, 1, 1)
+result = randomized_quick_sort(arr = arr, l = 1, r = 5)
+if (identical(result, c(1, 1, 2, 3, 9)) == TRUE) {
+  print("Test Passed.  The vector has been correctly sorted.")
+} else {
+  print("Test Failed.  The vector was not correctly sorted, or some other error occurred.")
+  print(result)
+}
